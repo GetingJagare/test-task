a4585081e0e5b00ffc5485b14cc9b698323cb8aa
GetingJagare <getingjagare@gmail.com>
2023-02-02 15:16:33 +0100

    app: migrations

---
+++ app/database/migrations/2023_02_02_141238_add_admin_role.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $adminRoleId = DB::table('user_roles')->select('id')->where('alias', 'admin')->first();
+        DB::table('users')->where('name', 'admin')->update(['role_id' => 1]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('users')->where('name', 'admin')->update(['role_id' => 0]);
+    }
+};
